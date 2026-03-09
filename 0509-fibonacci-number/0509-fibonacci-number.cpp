14a91a5e077f31df278128944ca7aadc900355e2
ADHITHIYA G <148076700+AdhithiyaG@users.noreply.github.com>
2026-03-09 21:52:48 +0530

    Time: 2 ms (78.77%), Space: 7.9 MB (19.67%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0||n==1){
+        int prev1 = 0;
+        int prev2 = 1;
+        int next = 0;
+        if(n==0 || n==1){
             return n;
         }
-        return fib(n-1)+fib(n-2);
+        for (int i = 2; i <= n; i++) {
+            next = prev2 + prev1;
+            prev1 = prev2;
+            prev2 = next;
+        }
+        return next;
     }
 };
