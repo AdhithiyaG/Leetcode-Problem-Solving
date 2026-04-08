c124886d4f70f8bdcc2268371156780c74b810a9
ADHITHIYA G <148076700+AdhithiyaG@users.noreply.github.com>
2026-04-08 20:33:51 +0530

    Time: 0 ms (100%), Space: 17.9 MB (22.75%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        // int n = nums.size();
+        // int maxProduct = 0;
+        // if(n==1){
+        //     return nums[0];
+        // }
+        // for (int i = 0; i < n; i++) {
+        //     int product = 1;
+        //     for (int j = i; j < n; j++) {
+        //         product *= nums[j];
+        //         maxProduct = max(product, maxProduct);
+        //     }
+        // }
+        // return maxProduct;
+
+        int prefixProduct = 1;
+        int suffixProduct = 1;
         int n = nums.size();
-        int maxProduct = 0;
-        if(n==1){
-            return nums[0];
-        }
+        int maxProduct = INT_MIN;
         for (int i = 0; i < n; i++) {
-            int product = 1;
-            for (int j = i; j < n; j++) {
-                product *= nums[j];
-                maxProduct = max(product, maxProduct);
+            prefixProduct *= nums[i];
+            maxProduct = max(prefixProduct, maxProduct);
+            if (nums[i] == 0) {
+                prefixProduct = 1;
+            }
+        }
+        for (int i = n-1; i >=0; i--) {
+            suffixProduct *= nums[i];
+            maxProduct = max(suffixProduct, maxProduct);
+            if (nums[i] == 0) {
+                suffixProduct = 1;
             }
         }
         return maxProduct;
