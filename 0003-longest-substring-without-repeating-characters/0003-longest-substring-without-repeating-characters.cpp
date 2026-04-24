98ff317cdf1f111e0a0229a11376a3b24a3c5c48
ADHITHIYA G <148076700+AdhithiyaG@users.noreply.github.com>
2026-04-24 20:58:49 +0530

    Time: 3 ms (80.98%), Space: 12 MB (46.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n=s.length();
-        int maxLen=INT_MIN;
-        int start=0;
-        for(int i=0;i<n;i++){
-            vector<int>vec(256,-1);
-            start=i;
-            for(int j=i;j<n;j++){
-                if(vec[s[j]]==1){
-                    break;
-                }
-                vec[s[j]]=1;
-                maxLen=max(maxLen,j-start+1);
+        // int n=s.length();
+        // int maxLen=INT_MIN;
+        // int start=0;
+        // for(int i=0;i<n;i++){
+        //     vector<int>vec(256,-1);
+        //     start=i;
+        //     for(int j=i;j<n;j++){
+        //         if(vec[s[j]]==1){
+        //             break;
+        //         }
+        //         vec[s[j]]=1;
+        //         maxLen=max(maxLen,j-start+1);
+        //     }
+        // }
+        // return maxLen==INT_MIN?0:maxLen;
+
+        int n = s.length();
+        int left = 0;
+        int right = 0;
+        int maxLen = INT_MIN;
+        unordered_map<char, int> mp;
+        for (int i = 0; i < n; i++) {
+            if (mp.find(s[i]) != mp.end() && mp[s[i]] >= left) {
+                left = mp[s[i]] + 1;
             }
+            mp[s[i]] = i;
+            maxLen = max(maxLen, right - left + 1);
+            right++;
         }
-        return maxLen==INT_MIN?0:maxLen;
+        return maxLen == INT_MIN ? 0 : maxLen;
     }
 };
